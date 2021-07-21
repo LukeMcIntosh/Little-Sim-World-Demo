7bcf4e15cce18741bd14885f34453da07be8d831
LukeMcIntosh <lukemcintosh99@gmail.com>
2021-07-21 16:35:11 +1200

    Basic Animations in place

--- LSM Unity Demo/Assets/Scripts/PlayerInput.cs
+++ LSM Unity Demo/Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator myAnimator = new Animator();
+    private Vector2 myMovementInput = new Vector2();
+
+    private Vector3 startScale;
+    private void Start()
     {
-        
+        startScale = transform.localScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+       
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            myAnimator.SetBool("W", true);
+            myAnimator.SetTrigger("Trigger W");
+
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            myAnimator.SetBool("W", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myAnimator.SetBool("S", true);
+            myAnimator.SetTrigger("Trigger S");
+
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            myAnimator.SetBool("S", false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.localScale = startScale;
+
+            myAnimator.SetBool("D", true);
+            myAnimator.SetTrigger("Trigger D");
+
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            myAnimator.SetBool("D", false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.localScale = new Vector3(startScale.x *-1, startScale.y, startScale.z);
+
+            myAnimator.SetBool("D", true);
+            myAnimator.SetTrigger("Trigger D");
+
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (!Input.GetKey(KeyCode.D))
+            {
+                myAnimator.SetBool("D", false);
+            }
+        }
+
     }
+
+
+
 }
