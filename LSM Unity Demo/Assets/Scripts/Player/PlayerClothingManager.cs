85dc87bc81a5a217ea6589b7b9ed3dedb8f5f1e9
LukeMcIntosh <lukemcintosh99@gmail.com>
2021-07-21 21:06:28 +1200

    New character sprites found/ proof of concept to how i will change clothes

---
+++ LSM Unity Demo/Assets/Scripts/Player/PlayerClothingManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClothingManager : MonoBehaviour
+{
+    public SpriteRenderer chestRenderer;
+
+    public List<Sprite> chestSprites = new List<Sprite>();
+
+    public void AssignActiveClotheSprite(int index)
+    {
+        chestRenderer.sprite = chestSprites[index];
+    }
+}
